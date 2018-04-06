976ed180950418dc5f21f7ed193da6403bc60766
Jake Bright <jbright32@my.stlcc.edu>
2018-04-06 14:54:35 -0500

    still working on project has issues, but i can render the form and recieve some error messages, not all

--- main.py
+++ main.py
@@ -1,55 +1,87 @@
 
-from flask import Flask, request, redirect, render_template, url_for
+from flask import Flask, request, redirect, render_template
 import cgi
 import os
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/welcome/', methods= ['GET'])
-def welcome():
-    import pdb; pdb.set_trace()       
-    
-    if not request.args.get('username', None):
-        return redirect('/signup/')
-    first_name = request.form['username']
-    return render_template('welcome.html', name= '')
-
-
-#@app.route('/signup/', methods= ['GET', 'POST'])
-#def display_signup_form():
-    
-@app.route('/signup/', methods= ['GET', 'POST'])
+@app.route('/signup', methods= ['GET', 'POST'])
 def sign_up():
-    import sys
 
-    error= ''
-    try: 
+    if request.method == 'POST':
 
-        if request.method == 'POST':
-            sys.stderr.write('this shit is confusing\n')
+        username = request.form['username']
+        password = request.form['password']
+        verifypassword = request.form['verify']
+        email = request.form['email']
+            
+        username_error= ''
+        password_error = ''
+        verify_error = ''
+        email_error = ''
 
-            username = request.form['username']
-            password = request.form['password']
-            verifypassword = request.form['verify password']
+        has_errors= False
 
-            username_error= ''
-            password_error= ''
-            print('verify username or password')      
-            if password == verifypassword:
-                _url = '/welcome/?username=%s' %username
-                import pdb; pdb.set_trace()       
+        # username verifacation
+        if len(username) > 20 or len(username) < 3:
+            username_error= 'Username must be between 3-20 characters'
+            username= ''
 
-                return redirect(_url)
+            has_errors== True
 
-            else:
-                error = 'passwords must match'
-        if request.method == 'GET':
-            return render_template('home.html')
+            return render_template('home.html', username_error= username_error)
 
-    except:
+        
+        #password verifacation
+        elif len(password) > 20 or len(password) < 3:
+            password_error= 'Password must be between 3-20 characters'
+            password= ''
+
+            has_errors== True
+            
+
+            return render_template('home.html', password_error= password_error)
+
+        elif password != verifypassword:
+            verify_error= 'Passwords must match'
+            verifypassword= ''
+
+            has_errors == True
+
+            return render_template('home.html', verify_error= verify_error)
+
+        elif has_errors == True:
+
+            return render_template('home.html', username_error= username_error, 
+                password_error= password_error, verify_error= verify_error, username= username,
+                password = password, verifypassword= verifypassword)
+        
+        else:    
+        
+            if not username_error and not password_error and not verify_error:
+                
+                return redirect('/welcome/?username={0}'.format(username))   #success message
+
+
+    else:
+                
+        return render_template('home.html')
+    
+    if request.method == 'GET':
+        return render_template('home.html')
+
+    
         return redirect('/welcome/')
 
-app.run()
+@app.route('/welcome/', methods= ['GET', 'POST'])
+def welcome():
+    if not request.args.get('username', None):
+        return redirect('/signup/')
+    first_name = request.args.get('username')
+    return render_template('welcome.html', name= first_name + '!')
 
+if __name__ == '__main__':
+    app.run()
 
