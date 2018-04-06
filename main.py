bb8424cb75c7eaab2f1abb371e880420c5b7b448
Jake Bright <jbright32@my.stlcc.edu>
2018-04-06 17:02:15 -0500

    Assignment is done, needs to look more pretty, add some css.

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ app.config['DEBUG'] = True
 @app.route('/signup', methods= ['GET', 'POST'])
 def sign_up():
 
+    has_errors= False
+
     if request.method == 'POST':
 
         username = request.form['username']
@@ -22,41 +24,77 @@ def sign_up():
         verify_error = ''
         email_error = ''
 
-        has_errors= False
-
-        # username verifacation
+        # username verifacatction
         if len(username) > 20 or len(username) < 3:
             username_error= 'Username must be between 3-20 characters'
             username= ''
+            
+            has_errors= True
 
-            has_errors== True
+        if ' ' in username:
+            username_error= 'Username cannot have spaces'
+            username= ''
 
-            return render_template('home.html', username_error= username_error)
-
+            has_errors= True
         
         #password verifacation
-        elif len(password) > 20 or len(password) < 3:
+        if len(password) > 20 or len(password) < 3:
             password_error= 'Password must be between 3-20 characters'
             password= ''
 
-            has_errors== True
+            has_errors= True
             
-
-            return render_template('home.html', password_error= password_error)
-
-        elif password != verifypassword:
+        if password != verifypassword:
             verify_error= 'Passwords must match'
             verifypassword= ''
 
-            has_errors == True
+            has_errors = True
 
-            return render_template('home.html', verify_error= verify_error)
+        if len(verifypassword) < 1:
+            verify_error= 'Cannot leave blank'
+            verifypassword= ''
 
-        elif has_errors == True:
+            has_errors= True
 
+        if ' ' in password:
+            password_error= 'Password cannot contain spaces'
+            password= ''
+
+            has_errors= True
+
+        #email verifacation
+        if len(email) > 0: 
+            
+            if email.count('@') > 1 or email.count('@') < 1:
+                email_error= 'Invalid email'
+                email= ''
+
+                has_errors= True
+                
+            if email.count('.') > 1 or email.count('.') < 1:
+                email_error= 'Invalid email'
+                email= ''
+
+                has_errors= True
+
+            if ' ' in email:
+                email_error= 'Email cannot contain spaces'
+                email= ''
+
+                has_errors= True
+
+            if len(email) > 20 or len(email) < 3:
+                email_error= 'Email must be between 3-20 characters'
+                email= ''
+
+                has_errors= True
+
+        if has_errors == True:
+            
             return render_template('home.html', username_error= username_error, 
                 password_error= password_error, verify_error= verify_error, username= username,
-                password = password, verifypassword= verifypassword)
+                password = password, verifypassword= verifypassword,email_error= email_error,
+                email= email, form= request.form)
         
         else:    
         
@@ -64,13 +102,8 @@ def sign_up():
                 
                 return redirect('/welcome/?username={0}'.format(username))   #success message
 
-
-    else:
-                
-        return render_template('home.html')
-    
     if request.method == 'GET':
-        return render_template('home.html')
+        return render_template('home.html', form= {})
 
     
         return redirect('/welcome/')
